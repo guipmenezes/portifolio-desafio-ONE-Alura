6b1aa09c30eeda15127f5581ccb475de37156024
guipmenezes <guimenezesunb@gmail.com>
2022-07-08 20:07:38 -0300

    validação concluída

--- js/validacao.js
+++ js/validacao.js
@@ -1,12 +1,12 @@
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo
 
-    if(validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
-    }
-
     if(input.validity.valid) {
-        input.parentElement.classList.remove()
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ' '
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
@@ -20,8 +20,8 @@ const mensagemDeErro = {
         valueMissing: 'O campo de nome não pode estar em branco.'
     },
     email: {
-        valueMissing: 'O campo de senha não pode estar em branco.',
-        typeMissmatch: 'O email digitado não é válido.'
+        valueMissing: 'O campo de email não pode estar em branco.',
+        typeMismatch: 'O email digitado não é válido.'
     },
     assunto: {
         valueMissing: 'O campo de assunto não pode estar em branco.'
@@ -29,4 +29,16 @@ const mensagemDeErro = {
     mensagem: {
         valueMissing: 'O campo de mensagem não pode estar em branco.'
     }
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ' '
+    
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagemDeErro[tipoDeInput][erro]
+        }
+    })
+
+    return mensagem
 }
